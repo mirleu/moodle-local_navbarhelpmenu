36aa041a346d3392f78faddff91b449edc8a4dfb
Miriam Leung <miriam.leung@ucsf.edu>
2025-04-14 09:32:58 -0700

    Moodle 4.5 upgrade. Fixed version required value.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_navbarhelpmenu';
-$plugin->version = 2025032100;
-$plugin->requires = 2024100100;
+$plugin->version = 2025041400;
+$plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
